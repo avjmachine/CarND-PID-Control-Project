aa6f06722265b9380a1bed6cecb0edc443401cbc
vjmechwiz <35830668+vjmechwiz@users.noreply.github.com>
2020-03-12 23:52:50 +0530

    feat: proportional control without parameter tuning

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  pid.Init(1.,1.,1.);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -63,7 +64,7 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
+          steer_value = -1*cte/(speed+0.0001); 
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
